880f0663a426fc84b16c5160b7ba3fd643e0b20d
Arratsu10 <bang369953@hexai.co.kr>
2023-09-21 17:22:32 +0900

    Initial Commit

---
+++ autoCheck/autoCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  autoCheckApp.swift
+//  autoCheck
+//
+//  Created by 방성환 on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct autoCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
