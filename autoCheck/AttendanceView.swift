bfb04bf2342c68e952ec6f0c096caaf83c2f6108
bang10 <bang369953@gmail.com>
2023-09-24 18:10:59 +0900

    design attendance

---
+++ autoCheck/AttendanceView.swift
@@ -0,0 +1,18 @@
+//
+//  AttendanceView.swift
+//  autoCheck
+//
+//  Created by 방성환 on 2023/09/24.
+//
+
+import SwiftUI
+
+struct AttendanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttendanceView()
+}
---
+++ autoCheck/MyRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  MyRecordView.swift
+//  autoCheck
+//
+//  Created by 방성환 on 2023/09/24.
+//
+
+import SwiftUI
+
+struct MyRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyRecordView()
+}
