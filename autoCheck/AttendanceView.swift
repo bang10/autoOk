e1f1c49c7502e2b642ea10883ddb40d6b2834c4b
bangseonghwan <bang369953@gmail.com>
2023-10-07 00:51:17 +0900

    정상적으로 작동되는 코드 (예비 제출용)

--- autoCheck/AttendanceView.swift
+++ autoCheck/AttendanceView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import BackgroundTasks
 
 struct AttendanceView: View {
     @EnvironmentObject var connectionBeacon: ConnectBeacon
@@ -25,10 +24,6 @@ struct AttendanceView: View {
     @State var classroom: String = ""
     @State var grade: String = ""
     @State var subjectId: String? = ""
-    @State private var remainingTime: TimeInterval = 180.0 // 3분
-    @State private var timer: Timer?
-    @State var resc: String = ""
-    @State private var isServerRequesting = false
     
     init(studentId: Binding<String> = .constant("2018100249")) {
         _studentId = studentId
@@ -41,8 +36,7 @@ struct AttendanceView: View {
                     Text(lastAttendanceTime)
                     Spacer()
                 }
-
-                Text(resc)
+                
                 Section(content: {
                     if subject == "" {
                         Text("이번시간에 출석할 강의가 없어요.")
@@ -56,10 +50,6 @@ struct AttendanceView: View {
                                 .overlay(
                                     VStack (){
                                         if let subjectId = subjectId{
-                                            Text(String(format: "남은 시간 : %.0f초", remainingTime))
-                                                            .font(.largeTitle)
-                                                            .padding()
-                                                        
                                             Text(isCheckToStr)
                                                 .font(.system(size: 25))
                                                 .padding(.bottom, 15)
@@ -81,6 +71,8 @@ struct AttendanceView: View {
                                                     .font(.system(size: 25))
                                                     .padding(.bottom, 15)
                                             }
+                                        } else {
+                                            Text("이번시간에 출석할 강의가 없어요.")
                                         }
                                     }
                                         
@@ -92,9 +84,6 @@ struct AttendanceView: View {
                                 .overlay(
                                     VStack (){
                                         if let subjectId = subjectId{
-                                            Text(String(format: "남은 시간 : %.0f초", remainingTime))
-                                                            .font(.largeTitle)
-                                                            .padding()
                                             Text(isCheckToStr)
                                                 .font(.system(size: 25))
                                                 .padding(.bottom, 15)
@@ -135,101 +124,70 @@ struct AttendanceView: View {
             }
         }
         .onAppear(perform: {
-            getTodayStudyInfo()
-            attedanceAction()
-            resetTimer()
-            startTimer()
+            attendanceService.getTodayStudnetAttendaceInfo(studentId: studentId) { res, error in
+                if let res = res {
+                    subjectId = res.subjectId
+                    subject = res.subjectName ?? ""
+                    studyTime = res.scheduleTime ?? ""
+                    isAttendance = res.attendance ?? ""
+                    classroom = res.classroom ?? ""
+                    name = res.studentName
+                    grade = res.grade
+                    department = res.department
+                    attendaceTime = res.attendanceTime ?? ""
+                }
+            }
+            
+            lastAttendanceTime = timeSet.getFormattedDate()
+            
+            if subject != "", isAttendance != "출석" {
+                if connectionBeacon.beaconDetected {
+                    attendanceService.saveAttendance(param: SSAADto(subjectId: subjectId ?? "", studentId: studentId, studyTime: studyTime)) { res, error in
+                        if let res =  res {
+                            subject = res.subjectName ?? ""
+                            isAttendance = res.attendance ?? ""
+                        } else {
+                            alert.alert(message: "출석을 실패했어요.")
+                        }
+                    }
+                }
+            }
             
         })
         .navigationTitle("출석")
         .navigationBarTitleDisplayMode(.automatic)
         .refreshable {
-            resetTimer()
-            attedanceAction()
-            startTimer()
-        }
-    }
-    
-    func getTodayStudyInfo() {
-        attendanceService.getTodayStudnetAttendaceInfo(studentId: studentId) { res, error in
-            if let res = res {
-                subjectId = res.subjectId
-                subject = res.subjectName ?? ""
-                studyTime = res.scheduleTime ?? ""
-                isAttendance = res.attendance ?? ""
-                classroom = res.classroom ?? ""
-                name = res.studentName
-                grade = res.grade
-                department = res.department
-                attendaceTime = res.attendanceTime ?? ""
-            }
-        }
-        
-        lastAttendanceTime = timeSet.getFormattedDate()
-        attedanceAction()
-    
-    }
-    func attedanceAction() {
-        resc = "start attendance ation"
-        if subject != "", isAttendance != "출석" {
-            if connectionBeacon.beaconDetected {
-                attendanceService.saveAttendance(param: SSAADto(subjectId: subjectId ?? "", studentId: studentId, studyTime: studyTime)) { res, error in
-                    if let res = res {
-                        subject = res.subjectName ?? ""
-                        isAttendance = res.attendance ?? ""
-                        stopTimer()
-                        alert.alert(message: "출석을 성공적으로 처리했어요.")
-                    }
+            attendanceService.getTodayStudnetAttendaceInfo(studentId: studentId) { res, error in
+                if let res = res {
+                    subjectId = res.subjectId
+                    subject = res.subjectName ?? ""
+                    studyTime = res.scheduleTime ?? ""
+                    isAttendance = res.attendance ?? ""
+                    classroom = res.classroom ?? ""
+                    name = res.studentName
+                    grade = res.grade
+                    department = res.department
+                    attendaceTime = res.attendanceTime ?? ""
                 }
             }
-        }
-    }
-    
-    func startTimer() {
-        resc = "start timer"
-            // 이미 타이머가 실행 중이면 중단
-            timer?.invalidate()
             
-            // 1초마다 실행되는 타이머 설정
-            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-                if self.remainingTime > 0 {
-                    self.remainingTime -= 1.0
-                    if !isServerRequesting {
-//                        attedanceAction()
+            lastAttendanceTime = timeSet.getFormattedDate()
+            
+            if subject != "", isAttendance == "" {
+                if connectionBeacon.beaconDetected {
+                    attendanceService.saveAttendance(param: SSAADto(subjectId: subjectId ?? "", studentId: studentId, studyTime: studyTime)) { res, error in
+                        if let res =  res {
+                            subject = res.subjectName ?? ""
+                            isAttendance = res.attendance ?? ""
+                            alert.alert(message: "출석을 성공적으로 했어요.")
+                        } else {
+                            alert.alert(message: "출석을 실패했어요.")
+                        }
                     }
-                } else {
-                    // 타이머가 종료되면 중단
-                    timer.invalidate()
                 }
             }
         }
-        
-        // 타이머를 3분으로 리셋
-        func resetTimer() {
-            remainingTime = 180.0
-            timer?.invalidate()
-        }
-        
-        // 백그라운드 태스크 등록
-        func registerBackgroundTask() {
-            BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.example.backgroundtask", using: nil) { task in
-                // 백그라운드 태스크가 실행될 때 특정 코드를 실행
-                self.performBackgroundTask()
-                task.setTaskCompleted(success: true)
-            }
-        }
-        
-        // 백그라운드 태스크에서 실행할 특정 코드
-        func performBackgroundTask() {
-            // 백그라운드에서 실행할 코드를 여기에 작성
-            // 예를 들어 네트워킹 또는 데이터 동기화 작업 등을 수행할 수 있습니다.
-        }
-    
-    // 타이머 중지
-        func stopTimer() {
-            timer?.invalidate()
-            timer = nil
-        }
+    }
     
 }
 
